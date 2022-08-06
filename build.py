81d956d9dc57ef1776e8ae60149d854ad8335133
Carsten Rambow <developer@elomagic.de>
2022-08-06 12:32:57 +0200

    Build script added

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
 import os
 import shutil
+from src.configuration import APP_VER as VERSION
 
-_MPY_CROSS_FILE = 'mpy-cross'
+# I recommend to create a local symbolic link to the mpy-cross from the micropython project
+# Sample: ln -s /Users/carsten/projects/micropython/mpy-cross/mpy-cross mpy-cross
+_MPY_CROSS_FILE = './mpy-cross'
 
-_SOURCE_DIR = './src'
-_TARGET_DIR = './target'
-_TARGET_FILES_DIR = f'{_TARGET_DIR}/files'
+_PROJECT_DIR = '.'
+_SOURCE_DIR = f'{_PROJECT_DIR}/src'
+_TARGET_DIR = f'{_PROJECT_DIR}/target'
+_DIST_DIR = f'{_TARGET_DIR}/dist'
+_TARGET_FILES_DIR = f'{_DIST_DIR}/files'
 
 if os.path.exists(_TARGET_DIR):
     shutil.rmtree(_TARGET_DIR)
@@ -14,21 +19,26 @@ if not os.path.exists(_TARGET_FILES_DIR):
     print(f'Creating folder {_TARGET_FILES_DIR}')
     os.makedirs(_TARGET_FILES_DIR)
 
-# Copy src python files to target files dir
+# Copy src Python files to target files dir
 shutil.copytree(_SOURCE_DIR, _TARGET_FILES_DIR, dirs_exist_ok=True)
 
+print(f'Freezing Python files...')
 for f in os.listdir(_TARGET_FILES_DIR):
     if f.endswith('.py'):
         source_file = f'{_TARGET_FILES_DIR}/{f}'
         target_file = f'{source_file[:-3]}.mpy'
-        print(f'Freezing code of file {source_file} to {target_file}')
+        print(f'\tFreezing code of file {source_file} to {target_file}')
         if os.system(f'{_MPY_CROSS_FILE} {source_file}') != 0:
             raise RuntimeError(f'Unable to freeze code of file {source_file}')
         # Delete original py file
         os.remove(source_file)
 
+shutil.copy(f'{_PROJECT_DIR}/requirements.txt', f'{_DIST_DIR}/requirements.txt')
+shutil.copy(f'{_PROJECT_DIR}/Setup-Client.py', f'{_DIST_DIR}/Setup-Client.txt')
+
 # Package target files
-version = '0.0.1-SNAPSHOT'
-target_zip_file = f'weather-client-micropython {version}.zip'
+#version = '0.0.1-SNAPSHOT'
+target_zip_file = f'{_TARGET_DIR}/weather-client-micropython {VERSION}'
+print(f'Creating ZIP distribution file {target_zip_file}.zip...')
 shutil.make_archive(target_zip_file, 'zip', _TARGET_FILES_DIR)
 print('Build done. Please check results.')
