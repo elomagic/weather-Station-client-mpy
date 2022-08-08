c9bc565a2f692e230ceb3ec047f0b07e10028aff
C. Rambow <weather-client.dev@elomagic.de>
2022-08-08 17:02:36 +0200

    Build tasks added

--- build.py
+++ build.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 import shutil
 from src.python.version import APP_VER as VERSION
 
-# I recommend to create a local symbolic link to the mpy-cross from the micropython project
+# I recommend to create a local symbolic link to the mpy-cross compiled binary from the micropython project
 # Sample: ln -s /Users/carsten/projects/micropython/mpy-cross/mpy-cross mpy-cross
 _MPY_CROSS_FILE = './mpy-cross'
 
@@ -29,41 +29,46 @@ def get_value_of_option(args: [], option: str, default_value=None) -> str:
     return args[args.index(option) + 1]
 
 
-def deploy() -> None:
-    global _DIST_DIR
-
-    print('\n-------- DEPLOY --------')
-
-    port = get_value_of_option(sys.argv, '--port', get_value_of_option(sys.argv, '-p', 'COM7'))
-    baud = get_value_of_option(sys.argv, '--baud', get_value_of_option(sys.argv, '-b', '115200'))
-
-    print(f'Deploying folder \'{_TARGET_FILES_DIR}\' to {port}...')
-    if os.system(f'ampy --port {port} --baud {baud} put {_TARGET_FILES_DIR} /') != 0:
-        raise IOError('Unable to deploy files')
-
-
-def build() -> None:
+def clean() -> None:
     global _TARGET_DIR
-    global _TARGET_FILES_DIR
-    global _PROJECT_DIR
-    global _DIST_DIR
-    global _SOURCE_DIR
-    global _MPY_CROSS_FILE
 
-    print('\n-------- BUILD --------')
+    print('\n-------- CLEAN --------')
 
     if os.path.exists(_TARGET_DIR):
+        print(f'Cleaning folder {_TARGET_DIR}')
         shutil.rmtree(_TARGET_DIR)
+
+    os.makedirs(_TARGET_DIR)
+
+    print('Done.')
+
+
+def resources() -> None:
+    global _TARGET_FILES_DIR
+    global _SOURCE_DIR
+
+    print('\n-------- RESOURCES --------')
 
     if not os.path.exists(_TARGET_FILES_DIR):
         print(f'Creating folder {_TARGET_FILES_DIR}')
         os.makedirs(_TARGET_FILES_DIR)
 
     # Copy src Python files to target files dir
+    print(f'Copying files from \'{_SOURCE_DIR}\' to \'{_TARGET_FILES_DIR}\'')
     shutil.copytree(
         _SOURCE_DIR,
         _TARGET_FILES_DIR,
-        ignore=shutil.ignore_patterns('__pycache__', '*.pyc', 'tmp*'), dirs_exist_ok=True)
+        ignore=shutil.ignore_patterns('__pycache__', '*.pyc', 'tmp*'),
+        dirs_exist_ok=True)
+
+    print('Done.')
+
+
+def build() -> None:
+    global _TARGET_FILES_DIR
+    global _MPY_CROSS_FILE
+
+    print('\n-------- BUILD --------')
 
     print(f'Freezing Python files...')
     for f in os.listdir(_TARGET_FILES_DIR):
@@ -79,6 +84,15 @@ def build() -> None:
             # Delete original py file
             os.remove(source_file)
 
+    print('Done.')
+
+
+def package() -> None:
+    global _PROJECT_DIR
+    global _DIST_DIR
+
+    print('\n-------- PACKAGE --------')
+
     shutil.copy(f'{_PROJECT_DIR}/requirements.txt', f'{_DIST_DIR}/requirements.txt')
     shutil.copy(f'{_PROJECT_DIR}/Setup-Client.py', f'{_DIST_DIR}/Setup-Client.txt')
 
@@ -87,14 +101,45 @@ def build() -> None:
     print(f'Creating ZIP distribution file {target_zip_file}.zip...')
     shutil.make_archive(target_zip_file, 'zip', _DIST_DIR)
 
+    print('Done.')
 
+
+def deploy() -> None:
+    global _TARGET_FILES_DIR
+
+    print('\n-------- DEPLOY --------')
+
+    port = get_value_of_option(sys.argv, '--port', get_value_of_option(sys.argv, '-p', 'COM7'))
+    baud = get_value_of_option(sys.argv, '--baud', get_value_of_option(sys.argv, '-b', '115200'))
+
+    print(f'Deploying folder \'{_TARGET_FILES_DIR}\' to {port}...')
+    if os.system(f'ampy --port {port} --baud {baud} put {_TARGET_FILES_DIR} /') != 0:
+        raise IOError('Unable to deploy files')
+
+    print('Done.')
+
+
+clean_switch: bool = contains_option(sys.argv, 'clean')
+resources_switch: bool = contains_option(sys.argv, 'resources')
 build_switch: bool = contains_option(sys.argv, 'build')
+package_switch: bool = contains_option(sys.argv, 'package')
 deploy_switch: bool = contains_option(sys.argv, 'deploy')
 
-if build_switch or not deploy_switch:
+build_switch = build_switch or not (clean_switch or resources_switch or build_switch or package_switch or deploy_switch)
+
+if clean_switch or build_switch or resources_switch:
+    clean()
+
+if resources_switch or build_switch:
+    resources()
+
+if build_switch:
     build()
 
-if deploy_switch or not build_switch:
+if package_switch or build_switch:
+    package()
+
+if deploy_switch:
     deploy()
 
 print('\nBuild script done. Please check results.')
